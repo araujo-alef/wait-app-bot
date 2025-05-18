647c86e0048be0077116a6bf1726644aa35fb875
alef araujo <alef.araujo@fornecedores.sicoob.com.br>
2025-05-17 21:45:52 -0300

    feat: added dot env to project

--- db.js
+++ db.js
@@ -3,7 +3,14 @@ const admin = require('firebase-admin');
 var serviceAccount = require("./serviceAccountKey.json");
 
 module.exports = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // <-- isso é crucial
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+  }),
 });
 
 const db = admin.firestore();
